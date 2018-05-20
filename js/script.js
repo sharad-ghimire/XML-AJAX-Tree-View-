faafbbaeb3454c68426f8bed57877e4e18182aec
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-05-20 15:22:12 +1000

    Fetched data from XML

--- js/script.js
+++ js/script.js
@@ -10,14 +10,27 @@ function loadData() {
         dataType: 'xml',
         success: function(data) {
             $(data).find('CategoriesRoot Categories').each(function(){
-                var info ="<li> ID: "+ $(this).find('CategoryID').text()+'</li>';
-                info += "<li> CategoryName: "+ $(this).find('CategoryName').text()+'</li>';
-                info += "<li> Description: "+ $(this).find('Description').text()+'</li>';
-
-                $('ul').append(info);
+                var catt ="<li> ID: "+ $(this).find('CategoryID').text()+'</li>';
+                catt += "<li> CategoryName: "+ $(this).find('CategoryName').text()+'</li>';
+                catt += "<li> Description: "+ $(this).find('Description').text()+'</li>';
+                $('.cat').append(catt);
             });
-        },
+        }
     });
+    $.ajax({
+        url: 'xml/products.xml',
+        dataType: 'xml',
+        success: function(data) {
+            $(data).find('ProductsRoot Products').each(function(){
+                var info ="<li> ID: "+ $(this).find('ProductID').text()+'</li>';
+                info += "<li> Product Name: "+ $(this).find('ProductName').text()+'</li>';
+                info += "<li> CategoryID: "+ $(this).find('CategoryID').text()+'</li>';
+                info += "<li> Qunatity Per Unit: "+ $(this).find('QuantityPerUnit').text()+'</li>';
+                info += "<li> Unit Price: "+ $(this).find('UnitPrice').text()+'</li>';
+                $('.pro').append(info);
+            });
+        }
+    })
 }
 
 function tree() {
