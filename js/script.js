81ef967b13a956c0487ba433b1bf2dc0b39616b9
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-05-22 15:55:43 +1000

    figured out how to add arrays to DOM

--- js/script.js
+++ js/script.js
@@ -10,50 +10,59 @@
     var quantityPerUnit = [];
     var unitPrice = [];
 
-    $.ajax({
+    var data = $.parseXML($.ajax({
         url: 'xml/categories.xml',
         dataType: 'xml',
-        success: function(data) {
-            $(data).find('CategoriesRoot Categories').each(function(){
-                categoryID.push($(this).find('CategoryID').text());
-                categoryName.push($(this).find('CategoryName').text());
-                categoryDescription.push($(this).find('Description').text())
+        async: false
+    }).responseText);
+    $(data).find('CategoriesRoot Categories').each(function () {
+        categoryID.push($(this).find('CategoryID').text());
+        categoryName.push($(this).find('CategoryName').text());
+        categoryDescription.push($(this).find('Description').text())
 
-            });
+    });
 
-            console.log(categoryName.length);
-            for (var i = 0; i < categoryName.length; i++) {
-                document.getElementById("pro").append(categoryName[i]);
-            } 
-
-            
-            
-        }
+    var data2 = $.parseXML($.ajax({
+        url: 'xml/products.xml',
+        dataType: 'xml',
+        async: false
+    }).responseText);
+    $(data2).find('ProductsRoot Products').each(function () {
+        productID.push($(this).find('ProductID').text());
+        productName.push($(this).find('ProductName').text());
+        productCatID.push($(this).find('CategoryID').text());
+        quantityPerUnit.push($(this).find('QuantityPerUnit').text());
+        unitPrice.push($(this).find('UnitPrice').text());
     });
 
 
 
 
-//    var getProducts= function() {
-//         //AJAX for Products
-//         $.ajax({
-//             url: 'xml/products.xml',
-//             dataType: 'xml',
-//             success: function (data) {
-//                 $(data).find('ProductsRoot Products').each(function () {
-//                     productID.push($(this).find('ProductID').text());
-//                     productName.push($(this).find('ProductName').text());
-//                     productCatID.push($(this).find('CategoryID').text());
-//                     quantityPerUnit.push($(this).find('QuantityPerUnit').text());
-//                     unitPrice.push($(this).find('UnitPrice').text());
-//                 });
-//             }
-//         });
-//     }
+    function displayData() {
+        var products = document.getElementById("pro");
+        for (var i = 0; i < categoryName.length; i++) {
+            var list = "<ul><a>" + categoryName[i] + "</a></ul>";
+            products.innerHTML += list;
+            for(var i=0; i< productID.length; i++ ) {
+                var node = document.createElement("li");
+                var textnode = document.createTextNode(productName[i]);
+                node.appendChild(textnode);
+                document.getElementById("pro").appendChild(node);
+            }
+
+        }
+
+    }
+
+    function checkForCatPro() {
+        for (var i = 0; i < productCatID.length; i++) {
+            if (productCatID[i] == categoryID[i]) {
+                return productName[i];
+            }
+        }
 
 
+    }
 
 
-    
-   
-
+    displayData();
